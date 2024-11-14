905a8eae0616380e66ca73eea1efb850631ee848
Tomo Kunagisa <root@dead.blue>
2024-11-15 00:47:51 +0800

    Import WIP codes to git.

--- src/py115/cloud.py
+++ src/py115/cloud.py
@@ -1,43 +1,108 @@
 __author__ = 'deadblue'
 
-from typing import Any, Dict, Tuple, Union
-
-from py115.lowlevel import (
-    Client, CommonParams, api
+from typing import (
+    Any, Dict, Iterable, Tuple
 )
-from py115.lowlevel.api.app import AppType
-from py115.services import (
-    OfflineService, StorageService
+
+from py115.lowlevel import Client
+from py115.lowlevel.api import *
+from py115.lowlevel.types import (
+    CommonParams,
+    AppName, 
+    QrcodeClient, 
+    QrcodeStatus
 )
 from py115.types import (
-    Credential, QrcodeSession, QrcodeStatus
+    Credential, QrcodeSession, File, Task
 )
+
+
+class OfflineService:
+
+    _lac: Client
+    _lcp: CommonParams
+
+    def __init__(self, lac: Client, lcp: CommonParams) -> None:
+        self._lac, self._lcp = lac, lcp
+    
+    def list(self) -> Iterable[Task]:
+        page_num = 1
+        while True:
+            result = self._lac.call_api(OfflineListApi(page_num))
+            yield from result.tasks
+            page_num += 1
+            if page_num > result.page_count:
+                break
+
+    def add(self, *urls: str, save_dir_id: str | None = None):
+        if len(urls) == 0: return
+        self._lac.call_api(OfflineAddUrlsApi(
+            self._lcp, 
+            urls, 
+            save_dir_id=save_dir_id
+        ))
+
+    def delete(self, *task_ids: str, delete_files: bool = False):
+        if len(task_ids) == 0: return
+        self._lac.call_api(OfflineDeleteApi(
+            info_hashes=task_ids, 
+            delete_files=delete_files
+        ))
+
+
+class StorageService:
+
+    _lac: Client
+    _lcp: CommonParams
+
+    def __init__(self, lac: Client, lcp: CommonParams) -> None:
+        self._lac, self._lcp = lac, lcp
+    
+    def list_files(self, dir_id: str) -> Iterable[File]:
+        spec = FileListApi(
+            dir_id=dir_id,
+            offset=0,
+            limit=24
+        )
+        while True:
+            result = self._lac.call_api(spec)
+            yield from result.files
+            spec.offset += result.limit
+            if spec.offset >= result.count: break
+
+    def search_files(self, dir_id: str, keyword: str) -> Iterable[File]:
+        spec = FileSearchApi(
+                keyword=keyword,
+                dir_id=dir_id,
+                offset=0,
+                limit=24
+            )
+        while True:
+            result = self._lac.call_api(spec)
+            yield from result.files
+            spec.offset += result.limit
+            if spec.offset >= result.count: break
 
 
 class Cloud:
-    """115 cloud service.
+    """115 cloud service."""
 
-    Args:
-        credential (Any): Credential object.
-        protocol_kwargs (dict): Settings for underlying protocol client.
-    """
-
-    _llc: Client
-    """Low-level client"""
-    _cp: CommonParams
-    """Common parameters for low-level APIs."""
+    _lac: Client
+    _lcp: CommonParams
 
     def __init__(
             self, 
-            credential: Any = None, 
-            app_ver: Union[str, None] = None
+            credential: Any = None,
+            *,
+            app_ver: str | None = None
         ) -> None:
-        self._llc = Client()
+        self._lac = Client()
         # Get app version
         if app_ver is None:
-            app_ver = self._llc.call_api(api.AppVersionApi())
-        self._cp = CommonParams(app_ver)
-        self._llc.set_user_agent(f'Mozilla/5.0 115Desktop/{app_ver}')
+            avr = self._lac.call_api(AppVersionApi())
+            app_ver = avr.get(AppName.BROWSER_WINDOWS).version_code
+        self._lac.set_user_agent(f'Mozilla/5.0 115Browser/{app_ver}')
+        self._lcp = CommonParams(app_ver)
         # Set credential
         if credential is not None:
             self.import_credential(credential)
@@ -54,109 +119,105 @@ class Cloud:
         cookies = _convert_to_cookies(credential)
         if cookies is None:
             return False
-        self._llc.import_cookies(cookies)
-        return self._after_login()
+        self._lac.import_cookies(cookies)
+        self._after_login()
+        return True
 
-    def _after_login(self) -> bool:
-        try:
-            result = self._llc.call_api(api.UploadInfoApi())
-            self._cp.set_user_info(
-                user_id=result.user_id,
-                user_key=result.user_key
-            )
-            return True
-        except:
-            return False
+    def _after_login(self):
+        result = self._lac.call_api(UploadInfoApi())
+        self._lcp.set_user_info(
+            user_id=result.user_id,
+            user_key=result.user_key
+        )
 
-    def export_credentail(self) -> Union[Credential, None]:
+    def export_credentail(self) -> Credential | None:
         """Export current credentail from cloud instance.
 
         Return:
             py115.types.Credential: Credential object, or None when credential is invalid.
         """
-        cookies = self._llc.export_cookies()
+        cookies = self._lac.export_cookies()
         cred = Credential(
-            uid=cookies.get('UID'),
-            cid=cookies.get('CID'),
-            seid=cookies.get('SEID')
+            uid=cookies.get('UID', None),
+            cid=cookies.get('CID', None),
+            seid=cookies.get('SEID', None)
         )
         return cred if cred else None
 
-    def qrcode_login(self, app_type: AppType = AppType.WEB) -> QrcodeSession:
+    def qrcode_start(self, client: QrcodeClient = QrcodeClient.WEB) -> QrcodeSession:
         """Start a QRcode login session.
 
         Args:
-            target (py115.types.LoginTarget): Target to simulate login.
+            client (QrcodeClient): Client type to simulate login.
 
         Returns:
-            py115.types.QrcodeSession: QRcode login session.
+            QrcodeSession: QRcode login session.
         """
-        token = self._llc.call_api(api.QrcodeTokenApi(app_type))
-        qrcode_image = self._llc.fetch(
-            api.get_qrcode_image_url(app_type, token.uid)
+        token = self._lac.call_api(QrcodeTokenApi(client=client))
+        qrcode_image = self._lac.fetch(
+            qrcode_get_image_url(client, token.uid)
         )
         return QrcodeSession(
-            app_type=app_type,
+            client=client,
             uid=token.uid,
             time=token.time,
             sign=token.sign,
             image_data=qrcode_image
         )
 
-    def qrcode_poll(self, session: QrcodeSession) -> QrcodeStatus:
+    def qrcode_poll(self, session: QrcodeSession) -> bool:
         """Poll QRcode login status.
 
         Args:
-            session (py115.types.QrcodeSession): QRcode login session.
+            session (QrcodeSession): QRcode login session.
         
         Returns:
-            py115.types.QrcodeStatus: QRcode status.
+            bool: True when login is done.
         """
-        status = self._llc.call_api(api.QrcodeStatusApi(
-            uid=session._uid, 
-            time=session._time, 
+        status = self._lac.call_api(QrcodeStatusApi(
+            uid=session._uid,
+            time=session._time,
             sign=session._sign
         ))
-        if status == api.QrcodeStatus.ALLOWED:
-            self._llc.call_api(api.QrcodeLoginApi(
-                app_type=session._app_type, 
+        if status == QrcodeStatus.ALLOWED:
+            self._lac.call_api(QrcodeLoginApi(
+                client=session._client,
                 uid=session._uid
             ))
-            if self._after_login():
-                return QrcodeStatus.DONE
-            else:
-                return QrcodeStatus.FAILED
-        elif status == api.QrcodeStatus.EXPIRED:
-            return QrcodeStatus.EXPIRED
-        return QrcodeStatus.WAITING
+            self._after_login()
+            return True
+        elif status == QrcodeStatus.EXPIRED:
+            # TODO: Raise custom exception.
+            raise Exception('QRcode expired!')
+        return False
 
     def offline(self) -> OfflineService:
         """Get offline service.
 
         Return:
-            py115.services.OfflineService: Offline service instance.
+            OfflineService: Offline service instance.
         """
-        return OfflineService(llc=self._llc, cp=self._cp)
+        return OfflineService(self._lac, self._lcp)
 
     def storage(self) -> StorageService:
         """Get storage service.
 
         Return:
-            py115.services.StorageService: Storage service instance.
+            StorageService: Storage service instance.
         """
-        return StorageService(llc=self._llc, cp=self._cp)
+        return StorageService(self._lac, self._lcp)
 
     def lowlevel(self) -> Tuple[Client, CommonParams]:
         """Get lowlevel client and parameters
 
         Returns:
             py115.lowlevel.Client: Low-level client for API calling.
-            py115.lowlevel.CommonParams: Common parameters for low-leve API.
+            py115.lowlevel.types.CommonParams: Common-used parameters for low-leve API.
         """
-        return (self._llc, self._cp)
+        return (self._lac, self._lcp)
 
 
-def _convert_to_cookies(credential: Any) -> Union[Dict[str, str], None]:
+def _convert_to_cookies(credential: Any) -> Dict[str, str] | None:
     if isinstance(credential, Credential):
         return credential.to_dict()
     # Handle dict or object
--- src/py115/types.py
+++ src/py115/types.py
@@ -2,70 +2,65 @@ __author__ = 'deadblue'
 
 from dataclasses import dataclass
 from datetime import datetime
-from enum import IntEnum
-from typing import Dict, Optional
+from typing import Dict, Optional, TypeAlias
 
-from py115.lowlevel.api import (
-    AppType, FileObject, TaskObject
+from py115.lowlevel.types import (
+    QrcodeClient, 
+    FileInfo,
+    TaskStatus,
+    TaskInfo
 )
 
 
-class QrcodeStatus(IntEnum):
+File: TypeAlias = FileInfo
 
-    WAITING = 0
-    """QRCode is waiting for scanning."""
-
-    DONE = 1
-    """QRCode is scanned and approved by user."""
-
-    EXPIRED = -1
-    """QRCode is expired."""
-
-    FAILED = -2
-    """QRcode is declined by user."""
+Task: TypeAlias = TaskInfo
 
 
 class QrcodeSession:
     """QRcode login session."""
 
-    _app_type: AppType
+    _client: QrcodeClient
     _uid: str
     _time: int
     _sign: str
-
-    image_data: bytes
-    """QRcode image data"""
+    _image_data: bytes
 
     def __init__(
             self,
-            app_type: AppType, 
+            client: QrcodeClient, 
             uid: str, 
             time: int, 
             sign: str, 
             image_data: bytes
         ) -> None:
-        self._app_type = app_type
+        self._client = client
         self._uid = uid
         self._time = time
         self._sign = sign
-        self.image_data = image_data
+        self._image_data = image_data
+
+    @property    
+    def image_data(self) -> bytes:
+        """QRcode image data"""
+        return self._image_data
 
 
 @dataclass
 class Credential:
     """Credential contains required information to identify user."""
 
-    uid: str
+    uid: str | None
     """
     "UID" cookie value.
     """
 
-    cid: str
+    cid: str | None
     """
     "CID" cookie value.
     """
 
-    seid: str
+    seid: str | None
     """
     "SEID" cookie value.
     """
@@ -86,84 +81,6 @@ class Credential:
         return self.uid is not None \
             and self.cid is not None \
             and self.seid is not None
-
-
-class VideoDefinition(IntEnum):
-
-    SD = 1
-    """Standard definition."""
-
-    HD = 2
-    """High definition."""
-
-    FHD = 3
-    """Full high definition."""
-
-    FHD_1080P = 4
-    """Another full high definition."""
-
-    UHD_4K = 5
-    """Ultra high definition."""
-
-    ORIGINAL = 100
-    """Other definition."""
-
-
-@dataclass(init=False)
-class File:
-    """File represents a file or directory on cloud."""
-
-    is_dir: bool
-    """Indicate whether file is a directory."""
-
-    file_id: str
-    """Unique ID of the file or directory on cloud."""
-
-    parent_id: str
-    """File ID of the parent directory."""
-
-    name: str
-    """Base name."""
-
-    modified_time: datetime
-    """Last modified datetime."""
-
-    size: int = 0
-    """Size in bytes."""
-
-    sha1: Optional[str] = None
-    """SHA-1 hash of the file in HEX encoding."""
-
-    pickcode: Optional[str] = None
-    """Pickcode to download the file."""
-
-    media_duration: Optional[int] = None
-    """Media duration in seconds for audio/video file."""
-
-    video_definition: Optional[VideoDefinition] = None
-    """Video definition"""
-
-    def __new__(cls, obj: FileObject) -> "File":
-        ret = object.__new__(cls)
-
-        ret.is_dir = obj.is_dir
-        ret.file_id = obj.file_id
-        ret.parent_id = obj.parent_id
-        ret.name = obj.name
-        ret.size = obj.size
-        ret.sha1 = obj.sha1
-        ret.pickcode = obj.pickcode
-        ret.media_duration = obj.media_duration
-        if obj.video_definition is not None:
-            ret.video_definition = VideoDefinition(obj.video_definition)
-        ret.modified_time = datetime.fromtimestamp(obj.update_time)
-        return ret
-
-    def __str__(self) -> str:
-        if self.is_dir:
-            return f'{self.name}/'
-        else:
-            return self.name
 
 
 @dataclass(init=False)
@@ -254,66 +171,9 @@ class UploadTicket:
         return datetime.now() < self.expiration
 
 
-class TaskStatus(IntEnum):
-
-    RUNNING = 1
-    """Task is running."""
-
-    COMPLETE = 2
-    """Task is completely downloaded."""
-
-    FAILED = -1
-    """Task is failed."""
-
-
-@dataclass(init=False)
-class Task:
-    """Task represents an offline task."""
-
-    task_id: str
-    """Unique ID of the task."""
-
-    name: str
-    """Task name."""
-
-    size: int
-    """Total size to be downloaded."""
-
-    created_time: datetime
-    """Task created time."""
-
-    percent: float
-    """Precentage of the download, 0~100."""
-
-    status: TaskStatus
-    """Task status."""
-
-    file_id: str
-    """Downloaded file ID of the task, may be None if the task does not finish."""
-
-    file_is_dir: bool
-    """Is the downloaded file a directory."""
-
-    def __new__(cls, task_obj: TaskObject):
-        ret = object.__new__(cls)
-        ret.task_id = task_obj.info_hash
-        ret.name = task_obj.name
-        ret.size = task_obj.size
-        ret.created_time = task_obj.created_time
-        ret.percent = task_obj.percent
-        ret.status = TaskStatus(task_obj.status)
-        ret.file_id = task_obj.file_id
-        ret.file_is_dir = task_obj.file_id == task_obj.dir_id
-        return ret
-
-    def is_complete(self) -> bool:
-        """Check is task complete."""
-        return self.status == TaskStatus.COMPLETE
-
-    def is_failed(self) -> bool:
-        """Check is task failed."""
-        return self.status == TaskStatus.FAILED
-
-    def is_running(self) -> bool:
-        """Check is task still running."""
-        return self.status == TaskStatus.RUNNING
+__all__ = [
+    'QrcodeClient',
+    'File',
+    'TaskStatus',
+    'Task'
+]
